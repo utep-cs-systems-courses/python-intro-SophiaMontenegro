538399106d8c264ed510457434cb4595ce5ec795
SophiaMontenegro <sophiaazulmontenegro@gmail.com>
2024-02-19 10:01:40 -0700

    Still in progress

--- countWords.py
+++ countWords.py
@@ -5,18 +5,30 @@ import os
 def readWord(fd): #will return word
     word = ""
     completeWord = False 
-
+    count = 0#check if a new line
+    
     while(not completeWord):
         bytesRead = os.read(fd, 1)#reads a byte at a time
 
         if(len(bytesRead) == 0):#check if it read nothing
-            return word
+            count += 1
+            if(count == 2):
+                return word
+            else:
+                continue
         charRead = bytesRead.decode() #decode byte to string
 
         if(charRead.isalpha()): #checks if its a letter
             #add to word
             word = word + charRead
-        else:
+        elif(charRead == " "):
+            if(len(word) > 0):
+                completeWord = True
+            else:
+                #word = " "
+                #compleWord = True
+                continue
+        else:#other symbols
             completeWord = True
 
     return word
@@ -33,15 +45,18 @@ fileAble = True #to start while loop
 #read contents of file and add to dictionary
 while(fileAble):
     word = readWord(fd)#calls function
-
+    #print(word)
     if(len(word) == 0):#checks if its the end of file
         #fileAble = False
         break
     #check uppercase WHEN == when
-    #if word IS in dict add and set n to 1
-    if(word in dict.keys()):
+    #if word IS in dict increment n by 1
+    if(word == " "):
+        #do nothing
+        continue
+    elif(word in dict.keys()):
         dict[word] = dict[word] + 1
-        #if word NOT in dict increment n by 1
+        #if word NOT in dict add to dict with n as 1
     else:
         dict[word] = 1
 
@@ -49,7 +64,7 @@ while(fileAble):
 #close file
 os.close(fd)
 
-print(dict)
+#print(dict)
 
 #sort dictionary
 dictKeys = list(dict.keys())
@@ -60,7 +75,8 @@ sortedDict = {i: dict[i] for i in dictKeys}
 fd = os.open("output.txt", os.O_RDWR | os.O_CREAT) #create new file
 for word in sortedDict:
     #one word by line
-    line = str.encode(word + " " + str(sortedDict[word]))
+    line = str.encode(word + " " + str(sortedDict[word])+ "\n")
+    #print(line)
     os.write(fd, line)
 
 os.close(fd)
